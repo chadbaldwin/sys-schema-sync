67799460a3028f8d9f706a25c9ee641b25c90871
Chad Baldwin <cbaldwin@hchb.com>
2025-10-17 14:47:22 -0700

    Add new sync - sys.data_spaces

---
+++ DB/dbo/Tables/Sync Tables/_data_spaces.sql
@@ -0,0 +1,14 @@
+CREATE TABLE dbo._data_spaces (
+    _DatabaseID     int           NOT NULL CONSTRAINT FK__data_spaces__DatabaseID REFERENCES dbo.[Database] (_DatabaseID),
+    _CollectionDate datetime2     NOT NULL,
+    --
+    [name]          nvarchar(128) NOT NULL,
+    data_space_id   int           NOT NULL,
+    [type]          char(2)       NOT NULL,
+    [type_desc]     nvarchar(60)      NULL,
+    is_default      bit           NOT NULL,
+    is_system       bit               NULL,
+
+    INDEX CIX__data_spaces__DatabaseID_data_space_id UNIQUE CLUSTERED (_DatabaseID, data_space_id),
+);
+GO
