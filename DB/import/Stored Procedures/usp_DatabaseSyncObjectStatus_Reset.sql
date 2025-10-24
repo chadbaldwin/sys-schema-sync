00295d8356d8799c00cfbb952756eb851f06182a
Chad Baldwin <cbaldwin@hchb.com>
2025-10-23 20:28:02 -0700

    Add some manual reset procs

--- DB/import/Stored Procedures/usp_DatabaseSyncObjectStatus_Reset.sql
+++ DB/import/Stored Procedures/usp_DatabaseSyncObjectStatus_Reset.sql
@@ -1,20 +1,28 @@
 ﻿CREATE PROC import.usp_DatabaseSyncObjectStatus_Reset (
     @SyncObjectID int = NULL,
     @InstanceID int = NULL,
-    @DatabaseID int = NULL
+    @DatabaseID int = NULL,
+    @ResetChecksum bit = 0, -- Resetting the checksum will force the data to be refreshed from the database rather than doing a checksum compare
+    @ResetErrors bit = 0,
+    @Force bit = 0
 )
 AS
 BEGIN;
-    IF COALESCE(@SyncObjectID, @InstanceID, @DatabaseID) IS NULL
+    IF (COALESCE(@SyncObjectID, @InstanceID, @DatabaseID) IS NULL AND @Force = 0)
     BEGIN;
         THROW 51000, 'At least 1 paramerter needs to be populated as a filter', 1;
     END;
 
-    UPDATE s SET s.LastSyncCheck = '1900-01-01', s.LastSyncChecksum = NULL, s.LastSyncError = NULL, s.LastSyncErrorMessage = NULL
+    UPDATE s
+    SET   s.LastSyncCheck        = '1900-01-01 00:00:00.0000000'
+        , s.LastSyncChecksum     = IIF(@ResetChecksum = 1, NULL, s.LastSyncChecksum)
+        , s.LastSyncError        = IIF(@ResetErrors   = 1, NULL, s.LastSyncError)
+        , s.LastSyncErrorMessage = IIF(@ResetErrors   = 1, NULL, s.LastSyncErrorMessage)
+        , s.LastSyncWasError     = IIF(@ResetErrors   = 1, 0   , s.LastSyncWasError)
     FROM import.DatabaseSyncObjectStatus s
     WHERE   (s.SyncObjectID = @SyncObjectID OR @SyncObjectID IS NULL)
         AND (s._InstanceID  = @InstanceID   OR @InstanceID   IS NULL)
         AND (s._DatabaseID  = @DatabaseID   OR @DatabaseID   IS NULL)
-		AND s.LastSyncCheck <> '1900-01-01'
+        AND s.LastSyncCheck <> '1900-01-01 00:00:00.0000000'
     OPTION (RECOMPILE);
 END;
---
+++ DB/import/Stored Procedures/usp_DatabaseSyncObjectStatus_Reset_Friendly.sql
@@ -0,0 +1,39 @@
+CREATE PROC import.usp_DatabaseSyncObjectStatus_Reset_Friendly (
+    @SyncObjectName nvarchar(128) = NULL,
+    @InstanceName nvarchar(257) = NULL,
+    @DatabaseName nvarchar(128) = NULL
+)
+AS
+BEGIN;
+    IF (COALESCE(@SyncObjectName, @InstanceName, @DatabaseName) IS NULL)
+    BEGIN;
+        THROW 51000, 'At least 1 paramerter needs to be populated as a filter', 1;
+    END;
+
+    DECLARE @SyncObjectID int,
+            @InstanceID int,
+            @DatabaseID int;
+
+    SELECT @SyncObjectID = SyncObjectID FROM import.SyncObject WHERE SyncObjectName = @SyncObjectName;
+    SELECT @InstanceID   = _InstanceID  FROM dbo.Instance      WHERE InstanceName = @InstanceName;
+    SELECT @DatabaseID   = _DatabaseID  FROM dbo.[Database]    WHERE DatabaseName = @DatabaseName;
+
+    IF (@SyncObjectID IS NULL AND @SyncObjectName IS NOT NULL)
+    BEGIN;
+        THROW 51000, 'Invalid value for @SyncObjectName', 1;
+    END;
+
+    IF (@InstanceID IS NULL AND @InstanceName IS NOT NULL)
+    BEGIN;
+        THROW 51000, 'Invalid value for @InstanceName', 1;
+    END;
+
+    IF (@DatabaseID IS NULL AND @DatabaseName IS NOT NULL)
+    BEGIN;
+        THROW 51000, 'Invalid value for @DatabaseName', 1;
+    END;
+
+    EXEC import.usp_DatabaseSyncObjectStatus_Reset @SyncObjectID = @SyncObjectID,
+                                                   @InstanceID = @InstanceID,
+                                                   @DatabaseID = @DatabaseID;
+END;
