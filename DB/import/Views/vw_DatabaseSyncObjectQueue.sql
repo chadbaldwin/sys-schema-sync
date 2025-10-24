3b425170e36a79c14ebcba79163584000e0ba482
Chad Baldwin <cbaldwin@hchb.com>
2025-10-23 20:30:11 -0700

    Improvements to queueing process

--- DB/import/Views/vw_DatabaseSyncObjectQueue.sql
+++ DB/import/Views/vw_DatabaseSyncObjectQueue.sql
@@ -11,32 +11,39 @@ AS
   This avoids issues such as getting stuck running a very large instance with many databses while there's others waiting to run.
 */
 WITH limit AS (
-    SELECT TOP(500) so.DatabaseSyncObjectID
+    SELECT TOP(500) x._InstanceID, x._DatabaseID, x.SyncObjectID, x.[priority], x.age_group, x.age_rank, x.age_rank_rand
     FROM (
-        SELECT x.DatabaseSyncObjectID, x.[priority]
+        SELECT x._InstanceID, x._DatabaseID, x.SyncObjectID, x.[priority], x.age_group, x.age_rank
             /* Sorting by age group and _then_ NEWID() in order to add some randomization within the age group
-               This helps with breaking up strings of instances/databases that are clustered together and helps
-               with spreading the workload out over time */
-            , rn = ROW_NUMBER() OVER (PARTITION BY x.[priority] ORDER BY x.age_group, NEWID())
+                This helps with breaking up strings of instances/databases that are clustered together and helps
+                with spreading the workload out over time */
+            , age_rank_rand = ROW_NUMBER() OVER (PARTITION BY x.[priority] ORDER BY x.age_group, NEWID())
         FROM (
-            SELECT so.DatabaseSyncObjectID, x.[priority]
-                , age_group = NTILE(10) OVER (PARTITION BY x.[priority] ORDER BY so.LastSyncCheck)
+            SELECT so._InstanceID, so._DatabaseID, so.SyncObjectID, x.[priority]
+                , age_group = NTILE(10) OVER win
+                , age_rank = ROW_NUMBER() OVER win
             FROM import.vw_DatabaseSyncObject so
                 CROSS APPLY (
                     SELECT [priority] = CASE
-                                            WHEN so.LastSyncCheck IS NULL THEN 0
-                                            WHEN so.SyncStaleAgeMinutes <= FLOOR(DATEDIFF_BIG(SECOND, so.LastSyncCheck, SYSUTCDATETIME()) / 60.0) THEN 1
-                                            ELSE 2
+                                            WHEN so.LastSyncCheck = '1900-01-01 00:00:00.0000000' OR so.LastSyncCheck IS NULL THEN 0 -- Manual resets / New syncs
+                                            WHEN so.SyncStaleAgeMinutes <= FLOOR(DATEDIFF_BIG(SECOND, so.LastSyncCheck, SYSUTCDATETIME()) / 60.0) THEN 1 -- Aging syncs
+                                            ELSE 2 -- Everything else
                                         END
                 ) x
+            WINDOW win AS (PARTITION BY x.[priority] ORDER BY so.LastSyncCheck)
         ) x
-    ) so
-    WHERE so.[priority] < 2 OR (so.[priority] = 2 AND so.rn <= 50)
-    ORDER BY so.[priority], so.rn
+    ) x
+    WHERE x.[priority] IN (0,1)
+        OR (x.[priority] = 2 AND (x.age_rank <= 10 OR x.age_rank_rand <= 40)) -- Grab the top 10 oldest syncs as well as 50 random, there will be occasional overlap, but that's ok
+    ORDER BY x.[priority], x.age_rank_rand
 )
 SELECT so._InstanceID, so._DatabaseID, so.InstanceName, so.DatabaseName
     , so.SyncObjectID, so.SyncObjectName, so.SyncObjectLevelID
     , so.LastSyncChecksum, so.LastSyncTime, so.LastSyncCheck, so.LastSyncError, so.LastSyncErrorMessage
     , so.ImportTable, so.ImportProc, so.ImportType, so.ExportQueryPath, so.ChecksumQueryText
 FROM import.vw_DatabaseSyncObject so
-WHERE EXISTS (SELECT * FROM limit l WHERE l.DatabaseSyncObjectID = so.DatabaseSyncObjectID);
+WHERE EXISTS (
+        SELECT so._InstanceID, so._DatabaseID, so.SyncObjectID
+        INTERSECT
+        SELECT l._InstanceID, l._DatabaseID, l.SyncObjectID FROM limit l
+    );
