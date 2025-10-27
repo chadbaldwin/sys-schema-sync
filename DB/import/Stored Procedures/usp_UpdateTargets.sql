ddacd747af021228bfc546e980aa9765cf059e76
Chad Baldwin <cbaldwin@hchb.com>
2025-10-27 16:48:25 -0700

    Split targets configuration out to separate file

--- DB/import/Stored Procedures/usp_UpdateTargets.sql
+++ DB/import/Stored Procedures/usp_UpdateTargets.sql
@@ -9,17 +9,17 @@ BEGIN;
     CREATE TABLE #tmp_db (
         InstanceName nvarchar(128) NOT NULL,
         DatabaseName nvarchar(128) NOT NULL,
-    )
+    );
     ------------------------------------------------------------------------------
 
     ------------------------------------------------------------------------------
     INSERT INTO #tmp_db (InstanceName, DatabaseName)
     SELECT d.Instance, d.[Database]
-    FROM OPENJSON(@ServiceConfigJSON, '$.TargetDatabases')
+    FROM OPENJSON(@ServiceConfigJSON)
         WITH (
-            Instance nvarchar(128),
-            [Database] nvarchar(128)
-        ) d
+            Instance nvarchar(128) '$.InstanceName',
+            [Database] nvarchar(128) '$.DatabaseName'
+        ) d;
     ------------------------------------------------------------------------------
 
     ------------------------------------------------------------------------------
@@ -29,12 +29,19 @@ BEGIN;
         FROM #tmp_db
     ) AS n ON n.InstanceName = o.InstanceName
     WHEN MATCHED AND o.IsEnabled = 0
-        THEN UPDATE SET o.IsEnabled = 1
+    THEN UPDATE
+        SET o.IsEnabled = 1,
+            o.DisableDate = NULL
     WHEN NOT MATCHED BY TARGET
-        THEN INSERT (InstanceName) VALUES (n.InstanceName)
+    THEN INSERT (InstanceName)
+         VALUES (n.InstanceName)
     WHEN NOT MATCHED BY SOURCE AND o.IsEnabled = 1
-        THEN UPDATE SET o.IsEnabled = 0
-    OUTPUT $action, 'Deleted', Deleted.*, 'Inserted', Inserted.*;
+    THEN UPDATE
+        SET o.IsEnabled = 0,
+            o.DisableDate = SYSUTCDATETIME()
+    OUTPUT $action AS MergeAction
+        , DELETED._InstanceID AS d__InstanceID, DELETED.InstanceName AS d_InstanceName, DELETED.InsertDate AS d_InsertDate, DELETED.IsEnabled AS d_IsEnabled
+        , INSERTED._InstanceID AS i__InstanceID, INSERTED.InstanceName AS i_InstanceName, INSERTED.InsertDate AS i_InsertDate, INSERTED.IsEnabled AS i_IsEnabled;
 
     MERGE INTO dbo.[Database] WITH(HOLDLOCK) o
     USING (
@@ -43,11 +50,17 @@ BEGIN;
             JOIN dbo.Instance i ON i.InstanceName = t.InstanceName
     ) AS n ON n._InstanceID = o._InstanceID AND n.DatabaseName = o.DatabaseName
     WHEN MATCHED AND o.IsEnabled = 0
-        THEN UPDATE SET o.IsEnabled = 1
+    THEN UPDATE
+        SET o.IsEnabled = 1,
+            o.DisableDate = NULL
     WHEN NOT MATCHED BY TARGET
-        THEN INSERT (_InstanceID, DatabaseName) VALUES (n._InstanceID, n.DatabaseName)
+    THEN INSERT (_InstanceID, DatabaseName)
+         VALUES (n._InstanceID, n.DatabaseName)
     WHEN NOT MATCHED BY SOURCE AND o.IsEnabled = 1
-        THEN UPDATE SET o.IsEnabled = 0
-    OUTPUT $action, 'Deleted', Deleted.*, 'Inserted', Inserted.*;
+    THEN UPDATE
+        SET o.IsEnabled = 0,
+            o.DisableDate = SYSUTCDATETIME()
+    OUTPUT $action AS MergeAction
+        , DELETED._DatabaseID AS d__DatabaseID, DELETED._InstanceID AS d__InstanceID, DELETED.DatabaseName AS d_DatabaseName, DELETED.InsertDate AS d_InsertDate, DELETED.IsEnabled AS d_IsEnabled
+        , INSERTED._DatabaseID AS i__DatabaseID, INSERTED._InstanceID AS i__InstanceID, INSERTED.DatabaseName AS i_DatabaseName, INSERTED.InsertDate AS i_InsertDate, INSERTED.IsEnabled AS i_IsEnabled;
 END;
-GO
--- Service/update_targets.ps1
+++ Service/update_targets.ps1
@@ -1,10 +1,7 @@
 #Requires -PSEdition Core -Version 7.0 -Modules @{ ModuleName="dbatools"; ModuleVersion="2.1.7" }
 
-$config = gc "${PSScriptRoot}\appsettings.jsonc" -Raw  | ConvertFrom-Json
-
-# Converting back to JSON rather than using the source so we can remove comments.
-# PowerShell supports comments in JSON, but SQL Server does not
-$configJSON = $config | ConvertTo-Json
+$config = gc "${PSScriptRoot}\appsettings.jsonc" -Raw | ConvertFrom-Json
+$targets = gc "${PSScriptRoot}\targets.json" -Raw
 
 $conn = Connect-DbaInstance -ConnectionString $config.RepositoryDatabaseConnectionString
 
@@ -12,6 +9,9 @@ $params = @{
     SqlInstance = $conn
     CommandType = 'StoredProcedure'
     Query = 'import.usp_UpdateTargets'
-    SqlParameter = @{ 'ServiceConfigJSON' = $configJSON }
+    SqlParameter = @{ 'ServiceConfigJSON' = $targets }
 }
-Invoke-DbaQuery @params
+$result = Invoke-DbaQuery @params -As DataSet
+
+$result.Tables[0] | Format-Table -AutoSize
+$result.Tables[1] | Format-Table -AutoSize
