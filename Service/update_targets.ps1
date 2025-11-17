51a83fd80d8b98ba5f02622a214151a9c7de30d6
Chad Baldwin <cbaldwin@hchb.com>
2025-11-17 14:14:16 -0500

    Cleanup update targets script

--- Service/update_targets.ps1
+++ Service/update_targets.ps1
@@ -1,17 +1,27 @@
 #Requires -PSEdition Core -Version 7.0 -Modules @{ ModuleName="dbatools"; ModuleVersion="2.1.7" }
 
-$config = gc "${PSScriptRoot}\appsettings.jsonc" -Raw | ConvertFrom-Json
-$targets = gc "${PSScriptRoot}\targets.json" -Raw
+[CmdletBinding()]
+param ()
 
-$conn = Connect-DbaInstance -ConnectionString $config.RepositoryDatabaseConnectionString
+$ErrorActionPreference = 'Stop'
 
+$config = Get-Content -LiteralPath "${PSScriptRoot}\appsettings.jsonc" -Raw | ConvertFrom-Json
+$targets = Get-Content -LiteralPath "${PSScriptRoot}\targets.json" -Raw
+
+Write-Host 'Establishing connection with repository database...'
+$conn = Connect-DbaInstance -ConnectionString $config.RepositoryDatabaseConnectionString -ConnectTimeout 10
+
+Write-Host 'Updating targets'
 $params = @{
     SqlInstance = $conn
     CommandType = 'StoredProcedure'
     Query = 'import.usp_UpdateTargets'
-    SqlParameter = @{ 'ServiceConfigJSON' = $targets }
+    SqlParameter = @{ ServiceConfigJSON = $targets; Verbose = $true }
+    As = 'DataSet'
+    QueryTimeout = 10
+    EnableException = $true
 }
-$result = Invoke-DbaQuery @params -As DataSet
+$result = Invoke-DbaQuery @params -Verbose
 
 $result.Tables[0] | Format-Table -AutoSize
 $result.Tables[1] | Format-Table -AutoSize
