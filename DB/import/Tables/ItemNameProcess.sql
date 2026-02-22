c4b7df9a56b87a566d8e9dfa35af4372b5d522b4
Chad Baldwin <cbaldwin@hchb.com>
2026-02-22 14:46:58 -0800

    remove old comments

--- DB/import/Tables/ItemNameProcess.sql
+++ DB/import/Tables/ItemNameProcess.sql
@@ -19,9 +19,5 @@ CREATE TABLE import.ItemNameProcess (
 
     INDEX CIX_ItemNameProcess_ProcessKey_ID CLUSTERED (ProcessKey, ID),
     -- Indexes for supporting usp_CreateItems
-  --INDEX IX_ItemNameProcess_ProcessKey__DatabaseID_SchemaName_ObjectName_ObjectType NONCLUSTERED (ProcessKey, _DatabaseID, SchemaName, ObjectName, ObjectType), -- Supports inserting and updating dbo.[Object]
-  --INDEX IX_ItemNameProcess_ProcessKey__DatabaseID__ObjectID_IndexName              NONCLUSTERED (ProcessKey, _DatabaseID, _ObjectID, IndexName),               -- Supports inserting and updating dbo.[Index]
-    INDEX IX_ItemNameProcess_ProcessKey__DatabaseID__ObjectID_ColumnName             NONCLUSTERED (ProcessKey, _DatabaseID, _ObjectID, ColumnName),              -- Supports inserting and updating dbo.[Column]
-  --INDEX IX_ItemNameProcess_ProcessKey__DatabaseID__IndexID                         NONCLUSTERED (ProcessKey, _DatabaseID, _IndexID),                           -- Supports dbo.[Index] updates for IsDeleted
-  --INDEX IX_ItemNameProcess_ProcessKey__DatabaseID__ColumnID                        NONCLUSTERED (ProcessKey, _DatabaseID, _ColumnID)                           -- Supports dbo.[Column] updates for IsDeleted
+    INDEX IX_ItemNameProcess_ProcessKey__DatabaseID__ObjectID_ColumnName NONCLUSTERED (ProcessKey, _DatabaseID, _ObjectID, ColumnName), -- Supports inserting and updating dbo.[Column]
 );
