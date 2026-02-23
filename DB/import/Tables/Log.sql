02e94d0bc50ebaf956733ff88269039cc09934b3
Chad Baldwin <cbaldwin@hchb.com>
2026-02-23 13:11:30 -0800

    whitespace cleanup

--- DB/import/Tables/Log.sql
+++ DB/import/Tables/Log.sql
@@ -1,18 +1,18 @@
 CREATE TABLE import.[Log] (
-	LogID       bigint          NOT NULL IDENTITY,
+    LogID       bigint          NOT NULL IDENTITY,
     InsertDate  datetime2       NOT NULL CONSTRAINT DF_Log_InsertDate DEFAULT SYSUTCDATETIME(),
-    _InstanceID int					NULL,
-    _DatabaseID int					NULL,
+    _InstanceID int                 NULL,
+    _DatabaseID int                 NULL,
     [Message]   nvarchar(2047)  NOT NULL,
     TimeStart   datetime2           NULL,
     [RowCount]  bigint              NULL,
-    String1	    nvarchar(1000)      NULL,
-    String2	    nvarchar(1000)      NULL,
-    String3	    nvarchar(1000)      NULL,
+    String1     nvarchar(1000)      NULL,
+    String2     nvarchar(1000)      NULL,
+    String3     nvarchar(1000)      NULL,
 );
 GO
 -- Cluster by InsertDate first since that will be the most common query
 -- Then cluster by LogID to make ordering determinisitc
 CREATE UNIQUE CLUSTERED INDEX CIX_Log_InsertDate_LogID
-	ON import.[Log] (InsertDate, LogID)
-	WITH (DATA_COMPRESSION = PAGE);
+    ON import.[Log] (InsertDate, LogID)
+    WITH (DATA_COMPRESSION = PAGE);
--- Service/dependencies/SQL/sys.database_automatic_tuning_options.sql
+++ Service/dependencies/SQL/sys.database_automatic_tuning_options.sql
@@ -1,5 +1,5 @@
 SELECT _RowHash = CONVERT(binary(32), HASHBYTES('SHA2_256', (SELECT x.* FROM (SELECT NULL) n(n) FOR JSON AUTO)))
-	--
-	, x.*
+    --
+    , x.*
 FROM sys.database_automatic_tuning_options x
 OPTION (RECOMPILE);
